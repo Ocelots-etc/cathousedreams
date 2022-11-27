291729a870687160b0e1f6190bf82b09b2757b71
catdgomez <catherinedgomez@gmail.com>
2022-11-27 15:07:04 -0600

    add callback funct w/submit type for event listnr

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const init = () => {
+//** add event listener to the form element */
+  const inputForm = document.querySelector('form');
+
+//** the ""inputForm"" callback function will execute */
+//** and event is an object that will be passed */
+//** in as an argument when the event listener is triggered */
+  inputForm.addEventListener('submit', (event) => {  
+
+    }
 
 }
 //** add event listener */
