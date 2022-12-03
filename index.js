963272598e412a07576ff077f8fb19d8c6bf1caa
catdgomez <catherinedgomez@gmail.com>
2022-12-03 13:30:53 -0600

    capture user input display in console

--- index.js
+++ index.js
@@ -6,9 +6,21 @@ const init = () => {
 //** and event is an object that will be passed */
 //** in as an argument when the event listener is triggered */
   inputForm.addEventListener('submit', (event) => {  
+//** to prevent the default of the page from refreshing */
+//** permits me to do something beside refreshing the page */
+    event.preventDefault();
 
-    }
+//** access user input from event object in order to determine */
+//** the best path for accessing the children property's HTML collection */
+//** to display in the console all of the nested elements of the */
+//** of the event.target element */
+    console.log(event.target.children[1].value);
 
-}
+    //** access user input from event object */
+    
+
+    });
+
+};
 //** add event listener */
 document.addEventListener('DOMContentLoaded', init);
