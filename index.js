397cfce7cea1213175e8af4b0d74ba6eb0445599
catdgomez <catherinedgomez@gmail.com>
2022-12-05 23:06:48 -0600

    render date on page

--- index.js
+++ index.js
@@ -1,40 +1,47 @@
+const entryContainer = document.querySelector(".entryContainer")
+
 const init = () => {
-//** add event listener to the form element */
-  const inputForm = document.querySelector('form');
 
-//** the ""inputForm"" callback function will execute */
-//** and event is an object that will be passed */
-//** in as an argument when the event listener is triggered */
-  inputForm.addEventListener('submit', (event) => {  
-//** to prevent the default of the page from refreshing */
-//** permits me to do something beside refreshing the page */
-    event.preventDefault();
-
-//** access user input from event object in order to determine */
-//** the best path for accessing the children property's HTML collection */
-//** to display in the console all of the nested elements of the */
-//** of the event.target element */
-//** const input = document.querySelector('input#searchByID') */
-//** console.log(input.value); */
-    console.log(event.target.children[1].value);
-//** access user input from event object */
-
-//** basic fecth request to connect to json server */
-    fetch(`http://localhost:8000/dreams/${event.target.children[1].value}`)
+/** basic fetch request to connect to json server */
+    fetch(`http://localhost:8000/dreams`)
     .then(response => response.json())
     .then(data => {
-//** input my data in place of default values by creating elements to store */
-      const day = document.querySelector('section#dreamDetails h4')
-      const type = document.querySelector('section#dreamDetails h5')
-      const story = document.querySelector('section#dreamDetails p')
-//** setting the values of the innerText to match the values of data */
-      day.innerText = data.day
-      type.innerText = data.type
-      story.innerText = data.story
+
+      console.log(data)
+
+      data.forEach(entry => renderEntry(entry)) 
 
     });
-    });
-
 };
+
+/** cb function post upon submit 
+ * 
+ * function postEntry(entry) {
+ *  fetch(`http://localhost:8000/dream`, {
+ *    method: 'POST',
+ *    headers:{'Content-Type': 'application/json'},
+ *    body: JSON.stringify(entry)
+ * })
+ *    .then((response) => response.json())
+ *    .then((newEntry) => renderEntry(newEntry))
+ * }
+ * 
+*/
+
+function renderEntry({day, type, story}) {
+
+  const dayH3Tag = document.createElement("h3")
+  dayH3Tag.innerText = day
+
+
+
+  const entryCard1 = document.createElement("div1")
+
+  entryCard1.append(dayH3Tag)
+  entryContainer.append(entryCard1)
+  entryCard1.innerText = day
+
+}
+
 //** add event listener */
 document.addEventListener('DOMContentLoaded', init);
